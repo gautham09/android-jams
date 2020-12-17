ba40f6110e808dd4095550c8ab073063490d07f2
gautham09 <76109027+gautham09@users.noreply.github.com>
2020-12-17 17:48:06 +0530

    Update q3.kt

--- week-1/q3.kt
+++ week-1/q3.kt
@@ -2,6 +2,7 @@ fun absolute(num : Int): Int {
     return (if(num < 0) num*(-1) else num)
 }
 fun main() {
-    var answer: Int = absolute(-15)
+    val n = Integer.valueOf(readLine())
+    var answer: Int = absolute(n)
     print("$answer")
 }

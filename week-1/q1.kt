99b55da1f486b9b9f074a6e203fbe9a48730eeb1
gautham09 <76109027+gautham09@users.noreply.github.com>
2020-12-17 12:07:48 +0530

    Create q1.kt

---
+++ week-1/q1.kt
@@ -0,0 +1,8 @@
+fun main() {
+	val n = 3
+    var result = "positive"
+    if(n < 0) result = "negative"
+    if(n == 0) result = "neither negative nor positive"
+    println("$n is $result")
+    
+}

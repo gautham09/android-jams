e35b1f0b34b72981e9980a03bdce85e412055e88
gautham09 <76109027+gautham09@users.noreply.github.com>
2020-12-18 12:04:57 +0530

    Update q2.kt

--- week-1/q2.kt
+++ week-1/q2.kt
@@ -1,10 +1,9 @@
 fun main() {
- numbers: Int = ArrayOf<Int>(0 , 0, 0)
-  var max:Int = numbers[0]
-    
+ numbers: Int = ArrayOf<Int>(0 , 0, 0)   
   for (index in 0..2) {
         numbers[index] = Integer.valueOf(readLine())
   }
+ var max:Int = numbers[0]
   for (num in numbers){
         if(num > max) max = num
     }
